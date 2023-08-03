cabb388f6341c487b95bdd7b56427b7d30c21585
shyam <shyam>
2023-08-03 11:58:22 +0530

    commit

--- bin/example ifelse.dart
+++ bin/example ifelse.dart
@@ -1,5 +1,5 @@
 
-void main(){
+/*void main(){
   int a=34,b=76;
   if(a>b){
     print('a is greater');
@@ -7,6 +7,29 @@ void main(){
   else{
     print('b is greater');
   }
+}*/
+/*
+import 'dart:io';
 
+void main(){
+  print('write the num');
+  int num=int.parse(stdin.readLineSync()!);
+  if(num%2==0){
+    print('its odd');
+  }
+  else{
+    print('even');
+  }
+}*/
+import 'dart:io';
 
+void main(){
+  print('number');
+  int num=int.parse(stdin.readLineSync()!);
+  if(num>0){
+    print('positive');
+  }
+  else{
+    print('negative');
+  }
 }
