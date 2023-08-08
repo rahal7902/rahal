b4a0932dd8459f717a2ac23fbbddd5c9101f5164
shyam <shyam>
2023-08-08 12:05:00 +0530

    commit

---
+++ bin/list.dart
@@ -0,0 +1,52 @@
+void main() {
+  List<int> num = [15, 20, 30, 40, 50];
+  //it will be stored as 0  1  2  3  4
+  //                     15 20 30 40 50
+  print(num);
+  num.add(8); //can add new variables to list
+  print(num);
+  num[3] = 10; //to change the variable
+  print(num);
+  var num2 = [60, 70, 80,];
+  num.addAll(num2);//add all lists
+  print(num);
+  print(num.length);//to know the length of the list
+  num.remove(30);//remove specific variable
+  print(num);
+  num.removeAt(6);//remove specific position
+  print(num);
+  num.insert(2, 45);//insert at specific position
+  print(num);
+  var num3 = List.empty(growable: true);//add empty list them add variables
+  num3.add(25);
+  num3.add(55);
+  num3.add(67);
+  print(num3);
+  num3.addAll(num2);
+  print(num3);
+  var list1 = List.from(num3);
+  print(list1);
+  var list2 = List.generate(
+      4, (index) => 99, growable: true); //extra list creation
+  print(list2);
+
+  var list3 = List.generate(5, (index) => index * 2, growable: true);
+  print(list3);
+  var list4 = List.generate(5, (index) => index + 2, growable: true);
+  print(list4);
+  list4.removeRange(1, 3);
+  print(list4);
+  print(list4.join(" "));
+print(list4.join("/"));
+  print(list4.join());
+  list4.forEach((element) {
+    print(element);
+  });
+  if(list4.contains(3)){
+    print(' present');
+  }
+  else{
+    print('not present');
+  }
+}
+
